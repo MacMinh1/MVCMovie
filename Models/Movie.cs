c1019229ce3e9876822de7ba84ea88cefc9e1d9e
haiminhnguyen49@gmail.com <haiminhnguyen49@gmail.com>
2022-05-23 10:00:06 +0700

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Tên Phim")]
+        public string NameMovie { get; set; }
+
+        [Display(Name = "Khởi Chiếu")]
+        [DataType(DataType.Date)]
+        public DateTime Premiere { get; set; }
+
+        [Display(Name = "Thể Loại")]
+        public string Genre { get; set; }
+
+        [Display(Name = "Thời Lượng")]
+        public string Time { get; set; }
+
+        [Display(Name = "Giá Tiền")]
+        public decimal Price { get; set; }
+    }
+}
+
---
+++ Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCMovie.Data;
+using System;
+using System.Linq;
+namespace MVCMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCMovieContext(
+            serviceProvider.GetRequiredService<
+            DbContextOptions<MVCMovieContext>>()))
+            {
+                // Kiểm tra thông tin cuốn sách đã tồn tại hay chưa
+                if (context.Movie.Any())
+                {
+                    return; // Không thêm nếu cuốn sách đã tồn tại trong DB
+                }
+                context.Movie.AddRange(
+                new Movie
+                {
+                    NameMovie = "Batman: Vạch Trần Sự Thật",
+                    Premiere = DateTime.Parse("2022-04-06"),
+                    Time = "183 phút",
+                    Genre = "Hành Động-Kinh Điển",
+                    Price = 198000
+                },
+                new Movie
+                {
+                    NameMovie = "No Way Home",
+                    Premiere = DateTime.Parse("2022-4-3"),
+                    Genre = "Hành Động-Tình Cảm",
+                    Time = "192 phút",
+                    Price = 189000
+                }
+                );
+                context.SaveChanges();//lưu dữ liệu
+            }
+        }
+    }
+}
